e5b72f216da0f758ad314d82e921d28e138097b4
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-14 16:27:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
 const outerRing = document.querySelector(".outer-ring");
-const innerWave = document.querySelector(".inner-wave");
-const outerCircles = document.querySelectorAll(".outer-circle");
-
-function updateVisualizer(amplitude) {
-  const dashOffset = 880 - amplitude * 1200; // Adjust based on input
-
-  document.querySelectorAll(".outer-ring").forEach((ring, index) => {
-    ring.style.strokeDashoffset = dashOffset + index * 20; // Adjust the multiplier for spacing
-  });
-
-  innerWave.style.opacity = amplitude;
-  innerWave.style.transform = `scale(${amplitude})`;
-}
-
-function simulateVoiceInput() {
-  const maxAmplitude = 0.8;
-  const randomAmplitude = Math.random() * maxAmplitude;
