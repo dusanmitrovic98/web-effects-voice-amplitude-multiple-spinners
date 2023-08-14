8926d29e80a87488df59f30a8aebdd33868d2aa9
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-14 16:08:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const outerRing = document.querySelector(".outer-ring");
 const innerWave = document.querySelector(".inner-wave");
 const outerCircles = document.querySelectorAll(".outer-circle");
+
